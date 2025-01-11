c7b6581cf595cdbdd956515f0505259e61b5fe88
Ganesh Maganti <ganesh.maganti12@gmail.com>
2025-01-11 23:54:01 +0530

    replace CommonJS require with ES module import in Tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import scrollbarHide from "tailwind-scrollbar-hide";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -15,5 +16,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [scrollbarHide],
 };
