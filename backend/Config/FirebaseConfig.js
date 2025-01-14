3b898ce17424eaba3742f8cf1d51d0de4ba96939
Ganesh Maganti <ganesh.maganti12@gmail.com>
2025-01-14 16:29:56 +0530

    Removed the code for retrieving the service account key from the .env file and import service account key directly from JSON file

--- backend/Config/FirebaseConfig.js
+++ backend/Config/FirebaseConfig.js
@@ -1,6 +1,5 @@
 const admin = require("firebase-admin");
-require("dotenv").config();
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+const serviceAccount = require("../../knnectServiceAccountKey.json");
 
 // Firebase Admin SDK initialization
 // Initialize the Firebase Admin SDK using the service account credentials
